5948ff601c6b1ca5e0762b0d60f726ff605ce520
madumalt <madumalt@gmail.com>
2018-03-08 15:37:15 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+MYSQL_DB_CONFIG = {
+    'host': '104.198.0.87',
+    'port': 3306,
+    'user': 'root',
+    'password': 'cfcwm07',
+    'db': 'curw'
+}
+
+PSQL_DB_CONFIG = {
+    'host': '35.230.47.230',
+    'port': 5432,
+    'user': 'postgres',
+    'password': 'postgres',
+    'db': 'curw'
+}
+
+START_DATE_TIME = '2018-01-01 00:00:00'
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+pip install PyMySQL
+pip install psycopg2-binary
